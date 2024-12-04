36b96ea7b0310f68837f5e1060df76efb9055bbe
Clive Kimari <kimariclive@gmail.com>
2024-12-04 18:43:15 +0000

    Add project files.

---
+++ Models/Expense.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SpendSmart.Models
+{
+    public class Expense
+    {
+        public int Id { get; set; }
+
+        public Decimal Value { get; set; }
+
+        [Required] public string? Description { get; set; }
+
+
+    }
+}
